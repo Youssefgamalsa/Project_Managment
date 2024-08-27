71807f636c81e235253584c8c98ba718acefa842
Hazem Aamer <zicoaamer@gmail.com>
2024-08-27 16:11:07 +0300

    Added Auth Context and retrived data in userData

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,20 @@
+import { jwtDecode } from "jwt-decode"
+import { createContext, useEffect, useState } from "react"
+
+export let AuthContext = createContext()
+
+export function AuthContextProvider({ children }) {
+  const [userData, setUserData] = useState(null)
+
+  useEffect(() => {
+    const token = localStorage.getItem("token")
+    if (token) {
+      const decoded = jwtDecode(token)
+      setUser(decoded)
+    }
+  }, [])
+
+  return (
+    <AuthContext.Provider value={{ userData }}>{children}</AuthContext.Provider>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-import { ToastContainer } from "react-toastify";
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import App from "./App.jsx"
+import { ToastContainer } from "react-toastify"
+import { AuthContextProvider } from "./context/AuthContext.jsx"
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ToastContainer/>
+    <ToastContainer />
+    <AuthContextProvider>
       <App />
+    </AuthContextProvider>
   </StrictMode>
-);
+)
